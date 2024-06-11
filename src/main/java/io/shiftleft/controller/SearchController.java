22da83863282b979670d6cb1789948ec3d28b491
Gabriel Acevedo <gacevedo@gmail.com>
2024-06-11 13:28:19 -0500

    Fixing src/main/java/io/shiftleft/controller/SearchController.java for finding 80

--- src/main/java/io/shiftleft/controller/SearchController.java
+++ src/main/java/io/shiftleft/controller/SearchController.java
@@ -18,15 +18,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SearchController {
 
   @RequestMapping(value = "/search/user", method = RequestMethod.GET)
-  public String doGetSearch(@RequestParam String foo, HttpServletResponse response, HttpServletRequest request) {
-    java.lang.Object message = new Object();
-    try {
-      ExpressionParser parser = new SpelExpressionParser();
-      Expression exp = parser.parseExpression(foo);
-      message = (Object) exp.getValue();
-    } catch (Exception ex) {
-      System.out.println(ex.getMessage());
-    }
-    return message.toString();
-  }
-}
+	@RequestMapping(value = "/search/user", method = RequestMethod.GET)
+	public String doGetSearch(@RequestParam String foo, HttpServletResponse response, HttpServletRequest request) {
+		String message = "";
+		try {
+			ExpressionParser parser = new SpelExpressionParser();
+			Expression exp = parser.parseExpression(foo);
+			message = String.valueOf(exp.getValue());
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return message;
+	}
+
+
