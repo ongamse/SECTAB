230f197a90cb8ff34720eff264eb145bdcb03ce5
Gabriel Acevedo <gacevedo@gmail.com>
2024-06-11 11:40:53 -0500

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 87

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -277,7 +277,7 @@ public class CustomerController {
    * @return String
    * @throws IOException
    */
-  @RequestMapping(value = "/debug", method = RequestMethod.GET)
+	@RequestMapping(value = "/debug", method = RequestMethod.GET)
   public String debug(@RequestParam String customerId,
 					  @RequestParam int clientId,
 					  @RequestParam String firstName,
@@ -305,6 +305,7 @@ public class CustomerController {
 
     return customer1.toString().toLowerCase().replace("script","");
   }
+
 
 	/**
 	 * Debug test for saving and reading a customer
@@ -388,3 +389,4 @@ public class CustomerController {
 	}
 
 }
+
