0683d2c9f27fc9badc5356513b555507e8658dbf
Gabriel Acevedo <gacevedo@gmail.com>
2024-07-09 13:17:04 -0500

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 83

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -303,8 +303,9 @@ public class CustomerController {
     httpResponse.setHeader("Location", String.format("%s/customers/%s",
                            request.getContextPath(), customer1.getId()));
 
-    return customer1.toString().toLowerCase().replace("script","");
+    return customer1.toString().replace("script",""); // Removed toLowerCase()
   }
+
 
 
 
@@ -391,6 +392,7 @@ public class CustomerController {
 	}
 
 }
+
 
 
 
